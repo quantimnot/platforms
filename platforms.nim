f819fe9115b65645d4ad901ffa63d2cf0c2fe8d5
quantimnot <quantimnot@users.noreply.github.com>
2022-07-03 18:08:09 -0400

    Add initial test

--- platforms.nim
+++ platforms.nim
@@ -3,3 +3,18 @@
 
 import platforms_generate
 export OS, OSInfo, CPU, platforms_generate
+
+
+#======
+# Tests
+#======
+
+import pkg/isolated_test
+
+isolatedTest:
+  import std/unittest
+  import "."/platforms_define
+  inclOS other, windows, "Some other Windows"
+  import "."/platforms_generate
+  check other.parents == {windows}
+  check other.info.name == "Some other Windows"
